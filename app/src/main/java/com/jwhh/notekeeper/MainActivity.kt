615ccac5b1a782ad32d8357a14b4b38e04f58aed
Tarek <tarek.buhdeima@gmail.com>
2019-07-01 20:47:37 +0300

    add junit tests

--- app/src/main/java/com/jwhh/notekeeper/MainActivity.kt
+++ app/src/main/java/com/jwhh/notekeeper/MainActivity.kt
@@ -1,24 +1,48 @@
 package com.jwhh.notekeeper
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity;
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
-
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
+    private val tag = this::class.simpleName
+    private var notePosition = POSITION_NOT_SET
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        val adapterCourses = ArrayAdapter<CourseInfo>(this, android.R.layout.simple_spinner_item,
+            DataManager.courses.values.toList())
+        adapterCourses.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinnerCourses.adapter = adapterCourses
+
+        notePosition = savedInstanceState?.getInt(NOTE_POSITION, POSITION_NOT_SET) ?:
+            intent.getIntExtra(NOTE_POSITION, POSITION_NOT_SET)
+
+        if (notePosition != POSITION_NOT_SET)
+            displayNote()
+        else{
+            createNewNote()
         }
+        Log.d(tag, "onCreate")
+    }
+
+    private fun createNewNote() {
+        DataManager.notes.add(NoteInfo())
+        notePosition = DataManager.notes.lastIndex
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(NOTE_POSITION, notePosition)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -33,7 +57,68 @@ class MainActivity : AppCompatActivity() {
         // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
             R.id.action_settings -> true
+            R.id.action_next -> {
+                if (notePosition < DataManager.notes.lastIndex) {
+                    moveNext()
+                } else {
+                    val message = "No more Notes"
+                    showMessage(message)
+                }
+                true
+            }
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    private fun displayNote() {
+        if (notePosition > DataManager.notes.lastIndex) {
+            val message = "Note not found"
+            showMessage(message)
+            Log.e(tag, "Invalid note position $notePosition," +
+                    " max valid position ${DataManager.notes.lastIndex}")
+            return
+        }
+        Log.i(tag, "Displaying text for position $notePosition")
+        val note = DataManager.notes[notePosition]
+        textNoteTitle.setText(note.title)
+        textNoteText.setText(note.text)
+
+        val coursePosition = DataManager.courses.values.indexOf(note.course)
+        spinnerCourses.setSelection(coursePosition)
+    }
+
+    private fun showMessage(message: String) {
+        Snackbar.make(textNoteTitle, message, Snackbar.LENGTH_LONG).show()
+    }
+
+    private fun moveNext() {
+        ++notePosition
+        displayNote()
+        invalidateOptionsMenu()
+    }
+
+    override fun onPrepareOptionsMenu(menu: Menu?): Boolean {
+        if (notePosition >= DataManager.notes.lastIndex) {
+            val menuItem = menu?.findItem(R.id.action_next)
+            if (menuItem != null) {
+                menuItem.icon = getDrawable(R.drawable.ic_block_white_24dp)
+                menuItem.isEnabled = false
+            }
+        }
+
+        return super.onPrepareOptionsMenu(menu)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        saveNote()
+        Log.d(tag, "onPause")
+    }
+
+    private fun saveNote() {
+        val note = DataManager.notes[notePosition]
+        note.title = textNoteTitle.text.toString()
+        note.text = textNoteText.text.toString()
+        note.course = spinnerCourses.selectedItem as CourseInfo
     }
 }
--- app/src/main/java/com/jwhh/notekeeper/NoteKeeperData.kt
+++ app/src/main/java/com/jwhh/notekeeper/NoteKeeperData.kt
@@ -3,3 +3,10 @@ package com.jwhh.notekeeper
 /**
  * Created by Tarek on 29-Jun-19.
  */
+data class CourseInfo(val courseId: String, val title: String) {
+    override fun toString(): String {
+        return title
+    }
+}
+
+data class NoteInfo(var course: CourseInfo? = null, var title: String? = null, var text: String? = null)
